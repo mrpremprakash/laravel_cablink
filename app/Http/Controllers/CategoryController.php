307418178e9f9d1499bd2f2633776307c0923007
prem <mr.premprakash@gmail.com>
2016-01-26 16:08:47 +0530

    jaslkdfjsalkdf

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,17 +20,43 @@ class CategoryController extends BaseController
         return view('admin/category/list');
     }
 
-
-
     public function add() {
         return view('admin/category/add');
     }
-    public function lists() {
+
+    public function store() {
+
+        $modelObj = new Category();
+        $modelObj->parent_id = Request::input('parent_id');
+        $modelObj->name = Request::input('name');
+        $modelObj->status = 'active';
+        $modelObj->level = $this->getLevel(Request::input('parent_id'));
+
+        $modelObj->save();
+        return $this->lists($modelObj->product_id);
+
+    }
+
+    public function getLevel($parent_id) {
+        if(empty($parent_id)) {
+            return 0;
+        }
+        $category_detail = Category::select('level')
+            ->where('category_id', $parent_id)
+            ->first()
+        ;
+        return $category_detail->level + 1;
+    }
+
+    public function lists($category_id = 0) {
         $limit = Request::input('limit', 20);
         $term = Request::input('term');
         $query = Category::select('category_id', 'name', 'parent_id', 'status', 'level');
         if(!empty($term)) {
             $query->where('name', 'like', DB::raw("'%".$term."%'"));
+        }
+        if(!empty($category_id)) {
+            $query->where('category_id', '=', $category_id);
         }
         $items = $query->paginate();
         return response()->json($items);
